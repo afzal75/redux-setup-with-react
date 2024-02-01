2dc9af9e1aa59b621e6bfc6f4733ca8c10e37a75
afzal75 <afzalbhuiyan16@gmail.com>
2024-02-01 13:29:18 +0600

    full redux setup complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useDispatch, useSelector } from "react-redux"
-import { decrement, increment } from "./redux/feauters/counterSlice";
+import { decrement, increment, incrementByValue } from "./redux/feauters/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
-  const { count } = useSelector((state) => state.counter)
-  const dispatch = useDispatch();
+  const { count } = useAppSelector((state) => state.counter)
+  const dispatch = useAppDispatch();
 
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="flex border border-purple-600 rounded-md bg-slate-50 p-10">
         <button onClick={() => dispatch(increment())} className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">Increment</button>
+
+        <button onClick={() => dispatch(incrementByValue(5))} className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">Increment by 6</button>
         <h1 className="text-3xl mx-10">{count}</h1>
         <button onClick={() => dispatch(decrement())} className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white">Decrement</button>
       </div>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './feauters/counterSlice'
+import Logger from './middleware/Logger'
+// import logger from 'redux-logger'
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(Logger)
 })
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+console.log(store.getState())
+
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
 export type AppDispatch = typeof store.dispatch
